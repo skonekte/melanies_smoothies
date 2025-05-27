d226da6a6c5c466011c22f0f498d82497ac03d97
skonekte <sanjay.sabnis@konekte.io>
2025-05-27 15:48:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ if(ingredients_list):
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.subheader(fruit_chosen + ": Nutrition Information ")
         #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         fv.df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)      
        
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
