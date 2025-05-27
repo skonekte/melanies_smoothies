7855dcfa31512a50edbd21e0ee4e737d03759924
skonekte <sanjay.sabnis@konekte.io>
2025-05-26 23:03:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 # Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-
-
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
@@ -20,24 +17,16 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
-# st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect('Choose Up to 5 Ingredients',my_dataframe, max_selections=5)
 
 if(ingredients_list):
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
-
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
     
-    # st.write(ingredients_string)
-
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-
-    # st.write(my_insert_stmt)
 
     time_to_insert = st.button('Submit Order')
 
